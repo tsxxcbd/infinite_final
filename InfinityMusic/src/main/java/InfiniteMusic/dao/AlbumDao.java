7efedb32c54d7abeba092b0c6ba225ee644617fc
Zihautanejo <126685670+Zihautanejo@users.noreply.github.com>
2023-12-20 13:17:50 +0800

    Delete InfinityMusic directory

--- InfinityMusic/src/main/java/InfiniteMusic/dao/AlbumDao.java
+++
@@ -1,7 +0,0 @@
-package InfiniteMusic.dao;
-
-import InfiniteMusic.entity.Album;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface AlbumDao extends BaseMapper<Album> {
-}
--- InfinityMusic/src/main/java/InfiniteMusic/dao/MusicianDao.java
+++
@@ -1,9 +0,0 @@
-package InfiniteMusic.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import InfiniteMusic.entity.Musician;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MusicianDao extends BaseMapper<Musician> {
-}
